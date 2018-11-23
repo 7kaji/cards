9d7b47c1a917891750e3f2c72a0f7bdb89ac2dfc
7kaji <hirotonbuu@gmail.com>
2018-11-23 23:19:10 +0900

    go fmt

--- go/card.go
+++ go/card.go
@@ -8,6 +8,6 @@ import (
 type card string
 
 func newCard(suit string, number int) string {
-  formatted_number := fmt.Sprintf("% 2s", strconv.Itoa(number))
-	return formatted_number+" of "+suit
+	formatted_number := fmt.Sprintf("% 2s", strconv.Itoa(number))
+	return formatted_number + " of " + suit
 }
